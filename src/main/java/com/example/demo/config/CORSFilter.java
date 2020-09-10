c0a1b4a960eb385e2074466cd351720a504822de
chyoo <chyoo@itfact.co.kr>
2020-09-10 17:25:09 +0900

    사용자 리스트 생성,추가,삭제,조회 커밋

--- src/main/java/com/example/demo/config/CORSFilter.java
+++ src/main/java/com/example/demo/config/CORSFilter.java
@@ -1,7 +1,35 @@
 package com.example.demo.config;
 
-public class CORSFilter {
- public static void main(String[] args) {
-	System.out.println();
-}
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter{
+
+	private static final Logger logger = Logger.getLogger("CORSFilter.class");
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException { 
+		logger.info("doFilter()");
+		
+		HttpServletResponse res = (HttpServletResponse) response;		
+		
+		
+		// @CrossOrigin 어노테이션으로 대체 가능 (Spring 4.2↑)
+		res.setHeader("Access-Control-Allow-Origin" , "*");
+		res.setHeader("Access-Control-Allow-Credentials" , "true");
+		res.setHeader("Access-Control-Allow-Methods" , "POST, GET, PUT, OPTIONS, DELETE");		
+		res.setHeader("Access-Control-Allow-Max-Age" , "3600");
+		res.setHeader("Access-Control-Allow-Headers" , "X-Requested-With, Content-Type, uthorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+		
+		chain.doFilter(request, res);			
+		
+	}
 }
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,71 @@
 package com.example.demo.controller;
 
-public class UserController {
+import java.util.List;
+import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.vo.UserVO;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	private static final Logger logger = Logger.getLogger("UserController.class");
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	@GetMapping
+	public List<UserVO> userList(){	
+		return userMapper.userList();
+	}
+	
+	@PostMapping
+	public void insertUser(@RequestBody UserVO user) {
+		
+		logger.info("insertUser()");		
+		userMapper.insertUser(user);
+	}
+	
+	@GetMapping("/{id}")
+	public UserVO fetchUserByID(@PathVariable int id) {
+		
+		logger.info("fetchUserByID()");
+		UserVO fetchUser = userMapper.fetchUserByID(id);
+		
+		return fetchUser;
+	}
+	
+	@PutMapping("/{id}")
+	public void updateUser(@PathVariable int id, @RequestBody UserVO user) {
+		
+		logger.info("updateUser()");
+		UserVO updateUser = user;
+		
+		updateUser.setFirstName(user.getFirstName());
+		updateUser.setLastName(user.getLastName());
+		updateUser.setAge(user.getAge());
+		updateUser.setSalary(user.getSalary());
+		
+		userMapper.updateUser(updateUser);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteuser(@PathVariable int id) {
+		logger.info("deleteUser()");
+		userMapper.deleteUser(id);		
+	}
+	
 }
